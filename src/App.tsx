a8a0db2385f945701b06a0541dafbad0277bdf63
xandrlev <xandrlev@gmail.com>
2024-03-03 19:45:59 +0300

    add env

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>MY SUPER APP</h1>
+      {import.meta.env.VITE_API_KEY}{" "}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
